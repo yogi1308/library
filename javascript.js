b97e2e88b49493e97f1619c4aaec9827186cd479
Shreetej Hadge <shreetejhadge1308@gmail.com>
2024-12-24 00:27:47 -0700

    added books manually to test displayBooks added a way to display the cover of the book in js

--- javascript.js
+++ javascript.js
@@ -1,20 +1,35 @@
-Vagabond = new Book('Vagabond', 'Takehiko Inoue')
+Vagabond = new Book('Vagabond', 'Takehiko Inoue', false, true, true, 327, 327, 'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781421577449/vagabond-vol-37-9781421577449_hr.jpg')
+TheSlamDunk = new Book('The Slam Dunk', 'Takehiko Inoue', false, true, true, 276, 276, 'https://d28hgpri8am2if.cloudfront.net/book_images/cvr9781421533285_9781421533285_hr.jpg')
+Berserk = new Book('Berserk', 'Kentaro Miura', false, true, true, 364, 364, 'https://images.booksense.com/images/205/070/9781593070205.jpg')
+OnePiece = new Book('One Piece', 'Eiichiro Oda', false, true, true, 1000, 1000, 'https://comicvine.gamespot.com/a/uploads/scale_small/11161/111610434/8594314-9160985681-97840.jpg')
+SteelBallRun = new Book('Steel Ball Run', 'Hirohiko Araki', false, true, true, 95, 95, 'https://m.media-amazon.com/images/I/917WFqQD1mL._AC_UF1000,1000_QL80_.jpg')
+ChainsawMan = new Book('Chainsaw Man', 'Tatsuki Fujimoto', false, true, true, 97, 97, 'https://images.booksense.com/images/785/722/9781974722785.jpg')
+VinLandSaga = new Book('Vinland Saga', 'Makoto Yukimura', false, true, true, 54, 54, 'https://m.media-amazon.com/images/I/91+Qs9DaFZL.jpg')
+TheAlchemist = new Book('The Alchemist', 'Paulo Coelho', false, true, true, 197, 197, 'https://i.harperapps.com/hcuk/covers/9780007487943/x400.jpg?ph=harperreach_coming_soon.png')
+TheHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', false, true, true, 310, 310, 'https://m.media-amazon.com/images/I/712cDO7d73L.jpg')
+TheGreatGatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', false, true, true, 180, 180, 'https://m.media-amazon.com/images/I/61OTNorhqVS._AC_UF894,1000_QL80_.jpg')
+TheHungerGames = new Book('The Hunger Games', 'Suzanne Collins', false, true, true, 374, 374, 'https://m.media-amazon.com/images/I/71un2hI4mcL.jpg')
+TheLordOfTheRings = new Book('The Lord of the Rings', 'J.R.R. Tolkien', false, true, true, 1178, 1178, 'https://s26162.pcdn.co/wp-content/uploads/2017/05/the-lord-of-the-rings-book-cover.jpg')
+TheSilmarillion = new Book('The Silmarillion', 'J.R.R. Tolkien', false, true, true, 365, 365, 'https://m.media-amazon.com/images/I/71Gt0U59D3L._AC_UF894,1000_QL80_.jpg')
+TheTwoTowers = new Book('The Two Towers', 'J.R.R. Tolkien', false, true, true, 322, 322, 'https://m.media-amazon.com/images/I/71nNxfSvGnL._UF1000,1000_QL80_.jpg')
+TheFellowshipOfTheRing = new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', false, true, true, 398, 398, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoX5QopXUKl6j79ro89axxAkcC253ryvX7Gg&shttps://ic.pics.livejournal.com/tree_lady/14285127/10506/10506_600.jpg')
+TheReturnOfTheKing = new Book('The Return of the King', 'J.R.R. Tolkien', false, true, true, 347, 347, 'https://www.knjizara-dominovic.hr/upload/catalog/product/8086/249042_the-return-ofthe-king-the-lord-of-the-rings-book-3-return-of-the-king-vol3.jpg')
 
 
-displayBooks(Vagabond)
+// displayBooks(Vagabond)
 
-// const myLibrary = [Vagabond, TheAlchemist, TheHobbit, TheGreatGatsby, TheHungerGames, TheLordOfTheRings, TheSilmarillion, TheTwoTowers, TheFellowshipOfTheRing, TheReturnOfTheKing, TheHobbit, TheHungerGames, TheGreatGatsby, TheAlchemist, Vagabond];
+const myLibrary = [Vagabond, TheSlamDunk, Berserk, OnePiece, SteelBallRun, ChainsawMan, VinLandSaga, TheAlchemist, TheHobbit, TheGreatGatsby, TheHungerGames, TheLordOfTheRings, TheSilmarillion, TheTwoTowers, TheFellowshipOfTheRing, TheReturnOfTheKing];
 
-function Book(title, author) {
+function Book(title, author, unread, read, started, pages, chapters, cover) {
   // the constructor...
   this.title = title
   this.author = author
-//   this.unread = unread
-//   this.read = read
-//   this.started = started
-//   this.pages = pages
-//   this.chapters = chapters
-//   this.cover = cover
+  this.unread = unread
+  this.read = read
+  this.started = started
+  this.pages = pages
+  this.chapters = chapters
+  this.cover = cover
 }
 
 console.log(Vagabond)
@@ -24,7 +39,7 @@ function addBookToLibrary() {
 
 }
 
-function displayBooks(Vagabond) {
+function displayBooks(currBook) {
     const main = document.querySelector(".main")
 
     const book = document.createElement("div")
@@ -32,6 +47,9 @@ function displayBooks(Vagabond) {
 
     const cover = document.createElement("div")
     cover.classList.add("book-cover")
+    cover.style.backgroundImage = `url(${currBook.cover})`
+    cover.style.backgroundSize = "cover"
+    cover.style.backgroundPosition = "center"
     book.appendChild(cover)
 
     const details = document.createElement("div")
@@ -41,11 +59,11 @@ function displayBooks(Vagabond) {
     const title_author = document.createElement("div")
 
     const title = document.createElement("p")
-    title.textContent = Vagabond.title
+    title.textContent = currBook.title
     title_author.appendChild(title)
 
     const author = document.createElement("p")
-    author.textContent = Vagabond.author
+    author.textContent = currBook.author
     title_author.appendChild(author)
 
     details.appendChild(title_author)
@@ -67,5 +85,5 @@ function displayBooks(Vagabond) {
 
 }
 
-// myLibrary.forEach(book => {displayBooks(book)})
+myLibrary.forEach(book => {displayBooks(book)})
 
