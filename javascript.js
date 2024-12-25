4a6ed984f2c4414601288b5692a781413197654c
Shreetej Hadge <shreetejhadge1308@gmail.com>
2024-12-25 01:22:05 -0700

    added a delete button to each

--- javascript.js
+++ javascript.js
@@ -49,6 +49,25 @@ function displayBooks(currBook) {
     cover.style.backgroundImage = `url(${currBook.cover})`
     cover.style.backgroundSize = "cover"
     cover.style.backgroundPosition = "center"
+
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("deleteButton");
+
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("viewBox", "0 0 15 17.5");
+    svg.setAttribute("height", "17.5");
+    svg.setAttribute("width", "15");
+    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    svg.classList.add("icon");
+
+    const iconPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
+    iconPath.setAttribute("transform", "translate(-2.5 -1.25)");
+    iconPath.setAttribute("d", "M15,18.75H5A1.251,1.251,0,0,1,3.75,17.5V5H2.5V3.75h15V5H16.25V17.5A1.251,1.251,0,0,1,15,18.75ZM5,5V17.5H15V5Zm7.5,10H11.25V7.5H12.5V15ZM8.75,15H7.5V7.5H8.75V15ZM12.5,2.5h-5V1.25h5V2.5Z");
+    iconPath.setAttribute("id", "Fill");
+
+    svg.appendChild(iconPath);
+    deleteButton.appendChild(svg);
+    cover.appendChild(deleteButton);
     book.appendChild(cover)
 
     const details = document.createElement("div")
