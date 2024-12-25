55d138ae510bf1af4f16086297a8d3782b0c79bb
Shreetej Hadge <shreetejhadge1308@gmail.com>
2024-12-24 22:59:02 -0700

    made the form fully functional and submitable

--- javascript.js
+++ javascript.js
@@ -1,19 +1,19 @@
-Vagabond = new Book('Vagabond', 'Takehiko Inoue', false, true, false, 327, 327, 'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781421577449/vagabond-vol-37-9781421577449_hr.jpg')
-TheSlamDunk = new Book('The Slam Dunk', 'Takehiko Inoue', false, false, true, 276, 276, 'https://d28hgpri8am2if.cloudfront.net/book_images/cvr9781421533285_9781421533285_hr.jpg')
-Berserk = new Book('Berserk', 'Kentaro Miura', false, true, false, 364, 364, 'https://images.booksense.com/images/205/070/9781593070205.jpg')
-OnePiece = new Book('One Piece', 'Eiichiro Oda', false, true, false, 1000, 1000, 'https://comicvine.gamespot.com/a/uploads/scale_small/11161/111610434/8594314-9160985681-97840.jpg')
-SteelBallRun = new Book('Steel Ball Run', 'Hirohiko Araki', false, false, true, 95, 95, 'https://m.media-amazon.com/images/I/917WFqQD1mL._AC_UF1000,1000_QL80_.jpg')
-ChainsawMan = new Book('Chainsaw Man', 'Tatsuki Fujimoto', false, true, false, 97, 97, 'https://images.booksense.com/images/785/722/9781974722785.jpg')
-VinLandSaga = new Book('Vinland Saga', 'Makoto Yukimura', false, true, false, 54, 54, 'https://m.media-amazon.com/images/I/91+Qs9DaFZL.jpg')
-TheAlchemist = new Book('The Alchemist', 'Paulo Coelho', true, false, false, 197, 197, 'https://i.harperapps.com/hcuk/covers/9780007487943/x400.jpg?ph=harperreach_coming_soon.png')
-TheHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', true, false, false, 310, 310, 'https://m.media-amazon.com/images/I/712cDO7d73L.jpg')
-TheGreatGatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', true, false, false, 180, 180, 'https://m.media-amazon.com/images/I/61OTNorhqVS._AC_UF894,1000_QL80_.jpg')
-TheHungerGames = new Book('The Hunger Games', 'Suzanne Collins', true, false, false, 374, 374, 'https://m.media-amazon.com/images/I/71un2hI4mcL.jpg')
-TheLordOfTheRings = new Book('The Lord of the Rings', 'J.R.R. Tolkien', true, false, false, 1178, 1178, 'https://s26162.pcdn.co/wp-content/uploads/2017/05/the-lord-of-the-rings-book-cover.jpg')
-TheSilmarillion = new Book('The Silmarillion', 'J.R.R. Tolkien', true, false, false, 365, 365, 'https://m.media-amazon.com/images/I/71Gt0U59D3L._AC_UF894,1000_QL80_.jpg')
-TheTwoTowers = new Book('The Two Towers', 'J.R.R. Tolkien', true, false, false, 322, 322, 'https://m.media-amazon.com/images/I/71nNxfSvGnL._UF1000,1000_QL80_.jpg')
-TheFellowshipOfTheRing = new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', true, false, false, 398, 398, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoX5QopXUKl6j79ro89axxAkcC253ryvX7Gg&shttps://ic.pics.livejournal.com/tree_lady/14285127/10506/10506_600.jpg')
-TheReturnOfTheKing = new Book('The Return of the King', 'J.R.R. Tolkien', true, false, false, 347, 347, 'https://www.knjizara-dominovic.hr/upload/catalog/product/8086/249042_the-return-ofthe-king-the-lord-of-the-rings-book-3-return-of-the-king-vol3.jpg')
+Vagabond = new Book('Vagabond', 'Takehiko Inoue', 'Completed', 327, 'Chapters', 'https://d28hgpri8am2if.cloudfront.net/book_images/onix/cvr9781421577449/vagabond-vol-37-9781421577449_hr.jpg')
+TheSlamDunk = new Book('The Slam Dunk', 'Takehiko Inoue', 'Started', 276, 'Chapters', 'https://d28hgpri8am2if.cloudfront.net/book_images/cvr9781421533285_9781421533285_hr.jpg')
+Berserk = new Book('Berserk', 'Kentaro Miura', 'Completed', 364, 'Chapters', 'https://images.booksense.com/images/205/070/9781593070205.jpg')
+OnePiece = new Book('One Piece', 'Eiichiro Oda', 'Completed', 1000, 'Chapters', 'https://comicvine.gamespot.com/a/uploads/scale_small/11161/111610434/8594314-9160985681-97840.jpg')
+SteelBallRun = new Book('Steel Ball Run', 'Hirohiko Araki', 'Started', 95, 'Chapters', 'https://m.media-amazon.com/images/I/917WFqQD1mL._AC_UF1000,1000_QL80_.jpg')
+ChainsawMan = new Book('Chainsaw Man', 'Tatsuki Fujimoto', 'Completed', 97, 'Chapters', 'https://images.booksense.com/images/785/722/9781974722785.jpg')
+VinLandSaga = new Book('Vinland Saga', 'Makoto Yukimura', 'Completed', 54, 'Chapters', 'https://m.media-amazon.com/images/I/91+Qs9DaFZL.jpg')
+TheAlchemist = new Book('The Alchemist', 'Paulo Coelho', 'Unread', 197, 'Pages', 'https://i.harperapps.com/hcuk/covers/9780007487943/x400.jpg?ph=harperreach_coming_soon.png')
+TheHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 'Unread', 310, 'Pages', 'https://m.media-amazon.com/images/I/712cDO7d73L.jpg')
+TheGreatGatsby = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 'Unread', 180, 'Pages', 'https://m.media-amazon.com/images/I/61OTNorhqVS._AC_UF894,1000_QL80_.jpg')
+TheHungerGames = new Book('The Hunger Games', 'Suzanne Collins', 'Unread', 374, 'Pages', 'https://m.media-amazon.com/images/I/71un2hI4mcL.jpg')
+TheLordOfTheRings = new Book('The Lord of the Rings', 'J.R.R. Tolkien', 'Unread', 1178, 'Pages', 'https://s26162.pcdn.co/wp-content/uploads/2017/05/the-lord-of-the-rings-book-cover.jpg')
+TheSilmarillion = new Book('The Silmarillion', 'J.R.R. Tolkien', 'Unread', 365, 'Pages', 'https://m.media-amazon.com/images/I/71Gt0U59D3L._AC_UF894,1000_QL80_.jpg')
+TheTwoTowers = new Book('The Two Towers', 'J.R.R. Tolkien', 'Unread', 322, 'Pages', 'https://m.media-amazon.com/images/I/71nNxfSvGnL._UF1000,1000_QL80_.jpg')
+TheFellowshipOfTheRing = new Book('The Fellowship of the Ring', 'J.R.R. Tolkien', 'Unread', 398, 'Pages', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoX5QopXUKl6j79ro89axxAkcC253ryvX7Gg&shttps://ic.pics.livejournal.com/tree_lady/14285127/10506/10506_600.jpg')
+TheReturnOfTheKing = new Book('The Return of the King', 'J.R.R. Tolkien', 'Unread', 347, 'Pages', 'https://www.knjizara-dominovic.hr/upload/catalog/product/8086/249042_the-return-ofthe-king-the-lord-of-the-rings-book-3-return-of-the-king-vol3.jpg')
 
 
 // displayBooks(Vagabond)
@@ -21,15 +21,13 @@ TheReturnOfTheKing = new Book('The Return of the King', 'J.R.R. Tolkien', true, 
 
 const myLibrary = [Vagabond, TheSlamDunk, Berserk, OnePiece, SteelBallRun, ChainsawMan, VinLandSaga, TheAlchemist, TheHobbit, TheGreatGatsby, TheHungerGames, TheLordOfTheRings, TheSilmarillion, TheTwoTowers, TheFellowshipOfTheRing, TheReturnOfTheKing];
 
-function Book(title, author, unread, completed, started, pages, chapters, cover) {
+function Book(title, author, status, length, lengthType, cover) {
   // the constructor...
   this.title = title
-  this.author = author
-  this.unread = unread // if book has not been read it should be in red
-  this.completed = completed // if book has been read it should be in green
-  this.started = started // if book has been started it should be in blue
-  this.pages = pages
-  this.chapters = chapters
+  this.author = author 
+  this.status = status
+  this.length = length
+  this.lengthType = lengthType
   this.cover = cover
 }
 
@@ -67,12 +65,16 @@ function displayBooks(currBook) {
     author.textContent = currBook.author
     title_author.appendChild(author)
 
+    const length = document.createElement("p")
+    length.textContent = currBook.length + " " + currBook.lengthType
+    title_author.appendChild(length)
+
     const status = document.createElement("p")
-    if (currBook.unread) {
+    if (currBook.status === "Unread") {
         status.textContent = "Unread"
         status.style.color = "red"
     }
-    else if (currBook.started) {
+    else if (currBook.status === "Started") {
         status.textContent = "Started"
         status.style.color = "blue"
     }
@@ -80,6 +82,7 @@ function displayBooks(currBook) {
         status.textContent = "Completed"
         status.style.color = "green"
     }
+
 
     title_author.appendChild(status)
 
@@ -102,12 +105,6 @@ function displayBooks(currBook) {
 
 }
 
-myLibrary.forEach(book => {displayBooks(book)})
-
-
-
-
-
 
 const plusButton = document.querySelector(".plusButton");
 const closeButton = document.querySelector(".closeButton");
@@ -124,6 +121,24 @@ closeButton.addEventListener("click", () => {
   main.classList.remove("dialog-open"); // Remove class to resize container back
 });
 
+confirmBtn.addEventListener("click", (event) => {
+  event.preventDefault(); // We don't want to submit this fake form
+  const coverDialog = document.querySelector("input#cover");
+  const titleDialog = document.querySelector("input#title");
+  const authorDialog = document.querySelector("input#author");
+  const lengthDialog = document.querySelector("#length");
+  const lengthTypeDialog = document.querySelector('input[name="length"]:checked');
+  const readStatusDialog = document.querySelector("select");
+  let newBookName = titleDialog;
+  newBookName= new Book(titleDialog.value, authorDialog.value, readStatusDialog.value, lengthDialog.value, lengthTypeDialog.value, coverDialog.value);
+  console.log(newBookName);
+  myLibrary.push(newBookName);
+  myLibrary.forEach(book => {displayBooks(book)})
+  dialog.close(); // Have to send the select box value here.
+  main.classList.remove("dialog-open"); // Remove class to resize container back
+  document.querySelector("form").reset(); // Reset the form
+});
 
 
+myLibrary.forEach(book => {displayBooks(book)})
 
